64101d082260028f1efd24a0aaef687a679dd056
ryanbytes <45298254+ryanbytes@users.noreply.github.com>
2026-08-23 07:57:44 -0400

    Bump Relief to 0.2.2

--- android/ReliefSetup/build.gradle.kts
+++ android/ReliefSetup/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "app.relief.setup"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
     }
 
     sourceSets {
